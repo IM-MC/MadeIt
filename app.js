78025307da9c85dde1b09166142f999c6c166be4
IM-MC <matthewchung022@hotmail.com>
2016-12-30 22:37:15 -0500

    Update app.js

--- app.js
+++ app.js
@@ -14,12 +14,16 @@ app.controller('MainCtrl', [
       {title: 'post 5', upvotes: 4}
     ];
     $scope.addPost = function(){
-      if(!$scope.title || $scope.title == '')
+      if(!$scope.title || $scope.title === '')
         {return; }
-      $scope.posts.push({title: $scope.title, upvotes: 0});
-      $scope.title='';
+      $scope.posts.push({title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
+        $scope.title='';
+        $scope.link='';
     };
     $scope.incrementUpvotes = function(post){
       post.upvotes += 1;
-    };
+    }
 }]);
