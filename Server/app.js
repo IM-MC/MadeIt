1f851a9fc90532607d895536329431475c5e294b
IM-MC <matthewchung022@hotmail.com>
2017-01-21 00:23:49 -0500

    Update app.js

--- Server/app.js
+++ Server/app.js
@@ -19,12 +19,9 @@ var app = express();
 
 mongoose.connect('mongodb://localhost/news');
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -34,24 +31,29 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/users', users);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
-// error handler
-app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+app.use(function(err, req,res,next){
+	res.status(err.status || 500);
+	res.render('error',{
+		message: err.message,
+		error: {}
+	});
 });
-
-app.use(passportinitialize());
 
 module.exports = app;
