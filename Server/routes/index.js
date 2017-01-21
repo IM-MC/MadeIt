0dfec2bfbb698ab739b58ec215e7d4ad27008e2c
IM-MC <matthewchung022@hotmail.com>
2017-01-21 00:22:47 -0500

    Update index.js

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -1,80 +1,98 @@
 var express = require('express');
 var router = express.Router();
-var passport = require('passport);')
-var User = mongoose.model('User');
+var mongoose = require('mongoose');
+var passport = require('passport');
 var jwt = require('express-jwt');
 
-var mongoose = require('mongoose');
 var Post = mongoose.model('Post');
 var Comment = mongoose.model('Comment');
+var User = mongoose.model('User');
 
 var auth = jwt({secret: 'SECRET', userProperty: 'payload'});
 
-router.get('/posts', function(req, res, next){
-	Post.find(function(err, post){
-		if(err){return next(err); }
+router.param('post', function(req, res, next, id) {
+  var query = Post.findById(id);
 
-		res.json(posts);
-	});
+  query.exec(function (err, post){
+    if (err) { return next(err); }
+    if (!post) { return next(new Error('can\'t find post')); }
+
+    req.post = post;
+    return next();
+  });
 });
 
-router.post('/posts', auth, function(req,res,next){
-	var post = new Post(req.body);
-	post.author = req.payload.username;
+// Not tested
+router.param('comment', function(req, res, next, id) {
+  var query = Post.findById(id);
 
-	post.save(function(err,post){
-		if(err){ return next{err}; }
+  query.exec(function (err, comment){
+    if (err) { return next(err); }
+    if (!comment) { return next(new Error('can\'t find comment')); }
 
-		res.json(post);
-	});
+    req.comment = comment;
+    return next();
+  });
 });
 
-router.param('post', function(req,res,next,id){
-	var query = Post.findById(id);
-
-	query.exec(function(err,post){
-		if (err){return next(err);}
-		if(!post){return next(new Error("Can't find post")); }
-
-		req.post = post;
-		return next();
-	});
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
-router.get('/posts/:post',function(req,res){
-	req.post.populate('comments', function(err,post){
-		if(err) {return next(err); }
+router.get('/posts', function(req, res, next) {
+  Post.find(function(err, posts){
+    if(err){ return next(err); }
 
-		res.json(req.post);
-	});
+    res.json(posts);
+  });
 });
 
-router.put('/posts/:post/upvote', auth, function(req,res,next){
-	req.post.upvote(function(err,post){
-		if (err) {return next(err); }
+router.get('/posts/:post', function(req, res, next) {
+  req.post.populate('comments', function(err, post) {
+    if (err) { return next(err); }
 
-		res.json(post);
-	});
+    res.json(post);
+  });
+});
+router.post('/posts', auth, function(req, res, next) {
+  var post = new Post(req.body);
+  post.author = req.payload.username;
+
+  post.save(function(err, post){
+    if(err){ return next(err); }
+
+    res.json(post);
+  });
 });
 
-router.post('/posts/:post/comments', auth, function(req,res,next){
-	var comment = new Comment(req.body);
-	comment.post = req.post;
-	comment.author = req.payload.username;
+router.put('/posts/:post/upvote', auth, function(req, res, next) {
+  req.post.upvote(function(err, post){
+    if (err) { return next(err); }
 
-	comment.save(function(err,comment){
-		if(err){return next(err); }
-
-		req.post.comments.push(comment);
-		req.post.save(function(err,post){
-			if(err) {return next(err);}
-
-			res.json(comment);
-		});
-	});
+    res.json(post);
+  });
 });
 
-router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
+router.post('/posts/:post/comments', auth, function(req, res, next) {
+  var comment = new Comment(req.body);
+  comment.post = req.post;
+  comment.author = req.payload.username;
+
+  comment.save(function(err, comment){
+    if(err){ return next(err); }
+
+    req.post.comments.push(comment);
+    req.post.save(function(err, post) {
+      if(err){ return next(err); }
+
+      res.json(comment);
+    });
+  });
+});
+
+// Functionality not tested
+router.post('/posts/:post/comments/:comment/upvote', auth, function(req, res, next) {
   req.comment.upvote(function(err, post){
     if (err) { return next(err); }
 
@@ -82,44 +100,38 @@ router.put('/posts/:post/comments/:comment/upvote', auth, function(req, res, nex
   });
 });
 
-router.post('/register',function(req,res,next){
-	if(!req.body.username || !req.body.password){
-		return res.status(400).json({message: 'Please fill out all fields'});
-	}
+router.post('/register', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
 
-	var user = new User();
+  var user = new User();
 
-	user.username = req.body.username;
-	user.setPassword(req.body.password)
+  user.username = req.body.username;
 
-	user.save(function(err){
-		if(err){return next(err); }
+  user.setPassword(req.body.password)
 
-		return res.json({token: user.generateJWT()})
-	});
+  user.save(function (err){
+    if(err){ return next(err); }
+
+    return res.json({token: user.generateJWT()})
+  });
 });
 
-router.post('/login', function(req,res,next){
-	if(!req.body.username || !req.body.password){
-		return res.status(400).jason({message: 'Please fill out all fields'});
-	}
+router.post('/login', function(req, res, next){
+  if(!req.body.username || !req.body.password){
+    return res.status(400).json({message: 'Please fill out all fields'});
+  }
 
-	passport.authenticate('local',function(err,user,info){
-		if(err){return next(err);}
+  passport.authenticate('local', function(err, user, info){
+    if(err){ return next(err); }
 
-		if(user){
-			return res.json({token: user.generateJWT()});
-		} else{
-			return res.status(401).json(info);
-		}
-	}) (req,res,next);
-});
-
-
-
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+    if(user){
+      return res.json({token: user.generateJWT()});
+    } else {
+      return res.status(401).json(info);
+    }
+  })(req, res, next);
 });
 
 module.exports = router;
